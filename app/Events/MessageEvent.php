1d0a3d738cb7681a4674bd8ee03675e7a716cc69
Sohaib Ilyas <isohaibilyas@gmail.com>
2021-02-26 19:18:21 +0500

    asdf

--- app/Events/MessageEvent.php
+++ app/Events/MessageEvent.php
@@ -37,6 +37,6 @@ class MessageEvent implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new Channel('messages-7');
+        return new PrivateChannel('messages-7');
     }
 }
